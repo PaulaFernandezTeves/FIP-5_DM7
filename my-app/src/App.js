158991ca07a79459f54255522274503c87a18f75
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-08-29 13:17:56 -0500

    nuevo ruta iniciar sesión

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import Home from './components/Home/Home'
 import Appbar from './components/AppBar'
-
+import IniciarSesion from './components/IniciarSesion/IniciarSesion'
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <Appbar/>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/IniciarSesion" component={IniciarSesion}/>
+
       </Switch>
     </Router>
 
